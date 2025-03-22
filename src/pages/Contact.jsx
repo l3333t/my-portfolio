6b9ff60d031c9dc232851b72f4f3e62075dd79f3
Alidar <panaguzhiyev@gmail.com>
2025-03-22 20:49:39 +0500

    some new stuff

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,11 +2,14 @@ function Contact() {
   return (
     <div className="flex flex-col items-center justify-center flex-1 text-center p-4">
       <div className="flex flex-col space-y-4 text-lg">
-        <a href="panaguzhiyev@gmail.com" className="text-gray-500 hover:text-white transition duration-200">
+        <a href="mailto:panaguzhiyev@gmail.com" className="text-gray-500 hover:text-white transition duration-200">
           Email
         </a>
         <a href="https://t.me/monogonal" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-white transition duration-200">
           Telegram
+        </a>
+        <a href="https://github.com/l3333t" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-white transition duration-200">
+          GitHub
         </a>
       </div>
     </div>
