1b528a5a284a23c260629e38a9fdc9dfc77d05d4
Alidar <panaguzhiyev@gmail.com>
2025-03-25 00:03:43 +0500

    Update Thoughts.jsx

--- src/pages/Thoughts.jsx
+++ src/pages/Thoughts.jsx
@@ -2,7 +2,7 @@ function Thoughts() {
   return (
     <div className="flex flex-col items-center justify-center flex-1 text-center p-4">
       <h2 className="text-3xl font-bold">
-        I don’t know where we’re going or why, but I just do human things to live a human life and not die.
+        Most interesting and big things start as a joke, but the difference between a joke and a revolution is the courage to take it seriously. The world doesn’t change by those who wait—it changes by those who dare to build, even when others laugh.
       </h2>
       <p className="text-gray-500 mt-4">Philosophical thoughts will be here soon—please wait a little.</p>
     </div>
